c3112266f4cfa771801549987f06bf298657e836
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-14 16:23:34 -0300

    arrumando bug de bigint do express

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+Object.defineProperty(BigInt.prototype, 'toJSON', {
+  get() {
+    return function (this: bigint) {
+      return this.toString();
+    };
+  },
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
