1db54b9a6cd90a15148c899403002a4d2808a694
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-15 15:37:38 -0300

    adicionando controlador de administrador

--- src/porto/porto.module.ts
+++ src/porto/porto.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PortoService } from './porto.service';
 import { PortoController } from './porto.controller';
 import { DatabaseModule } from 'src/database/database.module';
+import { AdminPortoController } from './admin.porto.controller';
 
 @Module({
-  controllers: [PortoController],
+  controllers: [AdminPortoController, PortoController],
   providers: [PortoService],
   imports: [DatabaseModule],
 })
