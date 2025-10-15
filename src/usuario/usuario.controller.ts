6a034eb18e598036bd57acc633a25c75fa0fd253
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-15 10:32:57 -0300

    recuperando informacoes do usuario logado

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  Request,
+} from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { ZodValidationPipe } from 'src/pipes/zod.validation.pipe';
 import {
   type CreateUsuarioDtoType,
   createUsuarioSchema,
 } from './dto/create-usuario.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('usuario')
 export class UsuarioController {
@@ -14,5 +23,12 @@ export class UsuarioController {
   @UsePipes(new ZodValidationPipe(createUsuarioSchema))
   create(@Body() createUsuarioDto: CreateUsuarioDtoType) {
     return this.usuarioService.create(createUsuarioDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  get(@Request() req) {
+    const userId = BigInt(req.user.sub);
+    return this.usuarioService.findById(userId);
   }
 }
