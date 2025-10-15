d76001635fa34ecdf19023da5613cf18eda0f212
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-15 15:11:15 -0300

    utilizando o partial do zod

--- src/ferry/dtos/update-ferry.dto.ts
+++ src/ferry/dtos/update-ferry.dto.ts
@@ -1,22 +1,6 @@
 import z from 'zod';
+import { createFerrySchema } from './create-ferry.dto';
 
-export const updateFerrySchema = z.object({
-  nome: z
-    .string('tipo inválido')
-    .max(100, 'nome muito grande, utilize no máximo 100 caracteres')
-    .optional(),
-  maximoDePessoas: z
-    .number('tipo inválido')
-    .min(1, 'no mínimo 1 pessoa')
-    .optional(),
-  maximoDeVeiculosEmM2: z
-    .number('tipo inválido')
-    .max(100000000, 'número muito grande, no maximo 100000000.00')
-    .refine(
-      (value) => /^\d+\.\d{2}$/.test(value.toFixed(2)),
-      'número decimal com 2 casas inválido',
-    )
-    .optional(),
-});
+export const updateFerrySchema = createFerrySchema.partial();
 
 export type UpdateFerryDto = z.infer<typeof updateFerrySchema>;
