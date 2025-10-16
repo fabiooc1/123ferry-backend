a1ace423dc6e6d4ca3b8d7fa9cd21ef1f4c563e2
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 15:24:47 -0300

    convertendo numero que vem do body

--- src/rota/dtos/create-rota.dto.ts
+++ src/rota/dtos/create-rota.dto.ts
@@ -2,8 +2,8 @@ import z from 'zod';
 
 export const createRotaSchema = z.object({
   nome: z.string().max(100),
-  origemId: z.bigint().positive(),
-  destinoId: z.bigint().positive(),
+  origemId: z.coerce.bigint().positive(),
+  destinoId: z.coerce.bigint().positive(),
 });
 
 export type CreateRotaDto = z.infer<typeof createRotaSchema>;
