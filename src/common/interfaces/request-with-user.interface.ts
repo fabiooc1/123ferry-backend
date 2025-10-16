f46db9527362d850fcd9c51c0cfe7c9f6a2696c1
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 14:05:14 -0300

    tipando request com payload do jwt

---
+++ src/common/interfaces/request-with-user.interface.ts
@@ -0,0 +1,6 @@
+import { Request } from 'express';
+import { UserPayload } from 'src/auth/interfaces/user-payload.interface';
+
+export interface RequestWithUser extends Request {
+  user: UserPayload;
+}
