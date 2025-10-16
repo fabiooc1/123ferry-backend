f094c876fde9eceb5228b94eef11d1b319bafd15
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 15:39:57 -0300

    convertendo para data

--- src/viagem/dtos/create-viagem.dto.ts
+++ src/viagem/dtos/create-viagem.dto.ts
@@ -1,10 +1,10 @@
 import z from 'zod';
 
 export const createViagemSchema = z.object({
-  ferryId: z.bigint().positive(),
-  rotaId: z.bigint().positive(),
-  dataPartida: z.date(),
-  dataChegada: z.date(),
+  ferryId: z.coerce.bigint().positive(),
+  rotaId: z.coerce.bigint().positive(),
+  dataPartida: z.coerce.date(),
+  dataChegada: z.coerce.date(),
 });
 
 export type CreateViagemDto = z.infer<typeof createViagemSchema>;
