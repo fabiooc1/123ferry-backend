6bc24355d31b63bdde8bb7a1569291731d9e794b
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 14:05:43 -0300

    melhorando forma de validar dados

--- src/pipes/zod.validation.pipe.ts
+++ src/pipes/zod.validation.pipe.ts
@@ -1,18 +1,10 @@
-import {
-  PipeTransform,
-  BadRequestException,
-  ArgumentMetadata,
-} from '@nestjs/common';
+import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { ZodError, ZodSchema } from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
-  transform(value: unknown, metadata: ArgumentMetadata) {
-    if (metadata.type !== 'body') {
-      return value;
-    }
-
+  transform(value: unknown) {
     try {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
