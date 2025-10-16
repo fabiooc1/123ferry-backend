a1c6f5f58388ce6a9346e1254a2cf003fcb3c413
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 15:09:26 -0300

    aumentando a duração do token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '5m' },
+      signOptions: { expiresIn: '20m' },
     }),
   ],
   controllers: [AuthController],
