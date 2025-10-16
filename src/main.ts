bbd5e140ff9af4e2e11414e73fa1923bc26764a2
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-16 14:31:06 -0300

    documentando a API

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 Object.defineProperty(BigInt.prototype, 'toJSON', {
   get() {
@@ -11,6 +12,18 @@ Object.defineProperty(BigInt.prototype, 'toJSON', {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiConfig = new DocumentBuilder()
+    .setTitle('123Ferry API')
+    .setDescription('Destinada a venda de passagens de ferryboat')
+    .setVersion('1.0')
+    .addTag('123ferry')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, apiConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
