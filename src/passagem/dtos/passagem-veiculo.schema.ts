fe050eb772da2020451e17753d5a4cb786e497bd
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-28 16:10:31 -0300

    informando o CPF do motorista para vincular ao veiculo da passagem

--- src/passagem/dtos/passagem-veiculo.schema.ts
+++ src/passagem/dtos/passagem-veiculo.schema.ts
@@ -1,8 +1,10 @@
+import { cpfSchema } from 'src/usuario/dto/cpf.schema';
 import z from 'zod';
 
 export const pasagemVeiculoSchema = z.object({
   placa: z.string().min(10),
   veiculoId: z.coerce.bigint().positive(),
+  motoristaCpf: cpfSchema,
 });
 
 export type PassagemVeiculoDto = z.infer<typeof pasagemVeiculoSchema>;
