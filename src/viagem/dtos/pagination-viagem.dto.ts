bdfc40ed2bc35bad65b068e25632c0335319b635
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-30 16:36:48 -0300

    alterando logica

--- src/viagem/dtos/pagination-viagem.dto.ts
+++ src/viagem/dtos/pagination-viagem.dto.ts
@@ -1,9 +1,5 @@
 import z from 'zod';
 
-const now = () => {
-  return new Date();
-};
-
 export const paginationViagemSchema = z.object({
   page: z.coerce.number().int().positive().default(1),
   pageSize: z.coerce.number().int().positive().default(6),
@@ -11,11 +7,25 @@ export const paginationViagemSchema = z.object({
   dataPartida: z
     .union([z.string().datetime(), z.date()])
     .optional()
-    .default(now)
+    .default(() => new Date()) // Use uma função para o default ser dinâmico
     .pipe(z.coerce.date())
-    .refine((val) => val >= now(), {
-      message: 'A data de partida não pode ser anterior ao momento atual.',
-    }),
+    .refine(
+      (val) => {
+        const inputDate = val;
+        const now = new Date();
+
+        const startOfToday = new Date(
+          now.getFullYear(),
+          now.getMonth(),
+          now.getDate(),
+        );
+
+        return inputDate >= startOfToday;
+      },
+      {
+        message: 'A data de partida não pode ser anterior a hoje.',
+      },
+    ),
 });
 
 export type PaginationViagemDto = z.infer<typeof paginationViagemSchema>;
