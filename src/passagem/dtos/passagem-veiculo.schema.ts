8c8237c6a8e91017f5e6a35918ca33e265542011
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-02 17:47:57 -0300

    alterando tamanho da placa

--- src/passagem/dtos/passagem-veiculo.schema.ts
+++ src/passagem/dtos/passagem-veiculo.schema.ts
@@ -2,7 +2,7 @@ import { cpfSchema } from 'src/usuario/dto/cpf.schema';
 import z from 'zod';
 
 export const pasagemVeiculoSchema = z.object({
-  placa: z.string().min(10),
+  placa: z.string().min(8),
   veiculoId: z.coerce.number().positive(),
   motoristaCpf: cpfSchema,
 });
